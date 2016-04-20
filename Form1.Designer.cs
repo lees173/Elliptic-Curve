d86af116ef990a5a33ef7df71f58620baa19c4f2
sungchul lee <lsungchul@gmail.com>
2016-04-20 12:07:55 -0700

    Eliptic Curve C#  program with graph

---
+++ Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxSave = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxX1 = new System.Windows.Forms.TextBox();
+            this.textBoxX2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxRX = new System.Windows.Forms.TextBox();
+            this.GetRButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxY1 = new System.Windows.Forms.TextBox();
+            this.textBoxY2 = new System.Windows.Forms.TextBox();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.textBoxRY = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.AutoScroll = true;
+            this.zedGraphControl1.AutoSize = true;
+            this.zedGraphControl1.IsShowPointValues = true;
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 111);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(964, 464);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.MouseDownEvent += new ZedGraph.ZedGraphControl.ZedMouseEventHandler(this.zedGraphControl1_MouseUpEvent);
+            this.zedGraphControl1.Load += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(292, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Chart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Insert a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Insert b:";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(93, 19);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA.TabIndex = 4;
+            this.textBoxA.Text = "-1";
+            this.textBoxA.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(93, 46);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxB.TabIndex = 4;
+            this.textBoxB.Text = "0";
+            // 
+            // textBoxSave
+            // 
+            this.textBoxSave.Location = new System.Drawing.Point(163, 70);
+            this.textBoxSave.Name = "textBoxSave";
+            this.textBoxSave.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSave.TabIndex = 6;
+            this.textBoxSave.Text = "Elliptic Curves";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Save Graph Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(645, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(278, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "For Zoom in and out using Mouse Wheel";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(471, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 18);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "X1:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(471, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 18);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "X2:";
+            // 
+            // textBoxX1
+            // 
+            this.textBoxX1.Enabled = false;
+            this.textBoxX1.Location = new System.Drawing.Point(507, 19);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(52, 20);
+            this.textBoxX1.TabIndex = 4;
+            this.textBoxX1.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            // 
+            // textBoxX2
+            // 
+            this.textBoxX2.Enabled = false;
+            this.textBoxX2.Location = new System.Drawing.Point(507, 46);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.Size = new System.Drawing.Size(52, 20);
+            this.textBoxX2.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(686, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 18);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "R X3:";
+            // 
+            // textBoxRX
+            // 
+            this.textBoxRX.Enabled = false;
+            this.textBoxRX.Location = new System.Drawing.Point(741, 21);
+            this.textBoxRX.Name = "textBoxRX";
+            this.textBoxRX.ShortcutsEnabled = false;
+            this.textBoxRX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRX.TabIndex = 4;
+            this.textBoxRX.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            // 
+            // GetRButton
+            // 
+            this.GetRButton.Location = new System.Drawing.Point(866, 23);
+            this.GetRButton.Name = "GetRButton";
+            this.GetRButton.Size = new System.Drawing.Size(68, 33);
+            this.GetRButton.TabIndex = 1;
+            this.GetRButton.Text = "Get R";
+            this.GetRButton.UseVisualStyleBackColor = true;
+            this.GetRButton.Click += new System.EventHandler(this.GetRButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(415, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(144, 18);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Mouse click in graph";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(575, 18);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 18);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Y1:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(575, 46);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 18);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Y2:";
+            // 
+            // textBoxY1
+            // 
+            this.textBoxY1.Enabled = false;
+            this.textBoxY1.Location = new System.Drawing.Point(611, 19);
+            this.textBoxY1.Name = "textBoxY1";
+            this.textBoxY1.Size = new System.Drawing.Size(52, 20);
+            this.textBoxY1.TabIndex = 4;
+            this.textBoxY1.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            // 
+            // textBoxY2
+            // 
+            this.textBoxY2.Enabled = false;
+            this.textBoxY2.Location = new System.Drawing.Point(611, 46);
+            this.textBoxY2.Name = "textBoxY2";
+            this.textBoxY2.Size = new System.Drawing.Size(52, 20);
+            this.textBoxY2.TabIndex = 4;
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(507, 70);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(68, 33);
+            this.ResetButton.TabIndex = 1;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_click);
+            // 
+            // textBoxRY
+            // 
+            this.textBoxRY.Enabled = false;
+            this.textBoxRY.Location = new System.Drawing.Point(741, 47);
+            this.textBoxRY.Name = "textBoxRY";
+            this.textBoxRY.ShortcutsEnabled = false;
+            this.textBoxRY.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRY.TabIndex = 4;
+            this.textBoxRY.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(686, 49);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 18);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "R Y3:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 587);
+            this.Controls.Add(this.textBoxSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxY2);
+            this.Controls.Add(this.textBoxX2);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxY1);
+            this.Controls.Add(this.textBoxRY);
+            this.Controls.Add(this.textBoxRX);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxX1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.GetRButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Elliptic Curve";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxX1;
+        private System.Windows.Forms.TextBox textBoxX2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxRX;
+        private System.Windows.Forms.Button GetRButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxY1;
+        private System.Windows.Forms.TextBox textBoxY2;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.TextBox textBoxRY;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using ZedGraph;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            GraphPane Zd_Pen = zedGraphControl1.GraphPane;
+
+            Zd_Pen.Title.Text = "Elliptic Curves";
+            Zd_Pen.XAxis.Title.Text = "X";
+            Zd_Pen.XAxis.Scale.MinorStep = 0.5F;
+            Zd_Pen.XAxis.Scale.MajorStep = 0.5f;
+            Zd_Pen.XAxis.Scale.Min = -5.0F;
+            Zd_Pen.XAxis.Scale.Max = 5.0F;
+
+            Zd_Pen.YAxis.Title.Text = "Y";
+            Zd_Pen.YAxis.Scale.MinorStep = 0.5F;
+            Zd_Pen.YAxis.Scale.MajorStep = 0.5f;
+            Zd_Pen.YAxis.Scale.Min = -5.0f;
+            Zd_Pen.YAxis.Scale.Max = 5.0f;
+
+
+        }
+
+        private void zedGraphControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+        GraphPane Zd_Pen;
+        GraphPane Zd_Pen1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            double a = double.Parse(textBoxA.Text);
+            double b = double.Parse(textBoxB.Text);
+            
+            Zd_Pen = zedGraphControl1.GraphPane;
+            Zd_Pen.CurveList.Clear();
+            Zd_Pen.GraphObjList.Clear();
+            PointPairList list1 = new PointPairList();
+            for (double i = -300; i < 300; i = i + 0.01)
+            {
+                x = (double)i;
+                y = Math.Pow(Math.Pow(x, 3) + a * x + b, 0.5);
+                list1.Add(x, y);
+
+            }
+            for (double i = -300; i < 300; i = i + 0.01)
+            {
+                x = (double)i;
+                y = Math.Pow(Math.Pow(x, 3) + a * x + b, 0.5);
+
+                list1.Add(x, -y);
+            }
+
+
+
+
+            LineItem Curve1 = Zd_Pen.AddCurve("Elliptic Curves", list1, Color.Red, SymbolType.None);
+            Curve1.Line.Width = 1.0F;
+
+            //draw
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            zedGraphControl1.Refresh();
+
+            zedGraphControl1.MasterPane.GetImage().Save(textBoxSave.Text+".png", System.Drawing.Imaging.ImageFormat.Png);
+
+
+
+        }
+
+        private void textBoxA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private bool zedGraphControl1_MouseUpEvent(ZedGraphControl sender, MouseEventArgs e)
+        {
+
+          
+            
+            object nearestObject;
+            int index;
+            this.zedGraphControl1.GraphPane.FindNearestObject(new PointF(e.X, e.Y), this.CreateGraphics(), out nearestObject, out index);
+            if (nearestObject != null && nearestObject.GetType() == typeof(LineItem))
+            {
+                LineItem lineItem = (LineItem)nearestObject;
+
+                if(textBoxX1.Text.Equals("")){
+                    textBoxX1.Text = Convert.ToString(lineItem[index].X);
+                    textBoxY1.Text = Convert.ToString(lineItem[index].Y);
+                }
+                else if (textBoxX2.Text.Equals(""))
+                {
+                    textBoxX2.Text = Convert.ToString(lineItem[index].X);
+                    textBoxY2.Text = Convert.ToString(lineItem[index].Y);
+                }
+                
+
+
+                zedGraphControl1.Invalidate();
+            }
+            
+            return default(bool);
+        }
+
+        private void ResetButton_click(object sender, EventArgs e)
+        {
+
+
+            textBoxX1.Text = "";
+            textBoxX2.Text = "";
+            textBoxY1.Text = "";
+            textBoxY2.Text = "";
+        }
+
+        private void GetRButton_Click(object sender, EventArgs e)
+        {
+            double x, y,x3,y3;
+            double x1 = Convert.ToDouble(textBoxX1.Text);
+            double y1 = Convert.ToDouble(textBoxY1.Text);
+            double x2 = Convert.ToDouble(textBoxX2.Text);
+            double y2 = Convert.ToDouble(textBoxY2.Text);
+            string R = "";
+            double m = (y2 - y1) / (x2 - x1);
+            x3 = 0;
+            y3 = 0;
+
+
+            Zd_Pen1 = zedGraphControl1.GraphPane;
+           
+            PointPairList list1 = new PointPairList();
+            for (double i = -300; i < 300; i = i + 0.01)
+            {
+                x = (double)i;
+                y = m*(x-x1)+y1;
+                list1.Add(x, y);
+
+            }
+
+
+            LineItem Curve1 = Zd_Pen1.AddCurve("Line", list1, Color.Blue, SymbolType.None);
+            Curve1.Line.Width = 1.0F;
+
+            //draw
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            zedGraphControl1.Refresh();
+            if (x1 == x2 && y1 == y2)
+            {
+                x3 = Math.Pow(m, 2) - x1*2;
+                y3 = m * (2 * x1 - Math.Pow(m, 2) - x2) - y1;
+
+            }
+            else if (x1 == x2 && -y1 == y2)
+            {
+                R = "infinity O";
+            }
+            else
+            {
+
+                x3 = Math.Pow(m, 2) - x1 - x2;
+                y3 = m * (x1 - x3) - y1;
+            }
+
+            if (R.Equals(""))
+            {
+
+                textBoxRX.Text = Convert.ToString(x3);
+                textBoxRY.Text=Convert.ToString(y3);
+            }
+            else
+            {
+                textBoxRX.Text = R;
+            }
+
+            
+
+
+            
+
+        }
+
+    
+    }
+}
