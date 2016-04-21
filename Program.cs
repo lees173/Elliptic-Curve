2a5aa2c4f47af34bfe8ec1a6f8693c4ec56674a0
sungchul lee <lsungchul@gmail.com>
2016-04-21 09:26:40 -0700

    removed unnecessary parts.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace WindowsFormsApplication1
     static class Program
     {
         /// <summary>
-        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// main
         /// </summary>
         [STAThread]
         static void Main()
